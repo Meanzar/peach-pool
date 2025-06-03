a00f7a9c2f9446ceff590a774b11321e5ea3b241
YannDecelle <yann.decelle@edu.devinci.fr>
2025-06-03 16:42:40 +0200

    edited index.js

--- frontend_safe/src/index.js
+++ frontend_safe/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MetaMaskProvider } from "@metamask/sdk-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MetaMaskProvider
+      debug={false}
+      sdkOptions={{
+        dappMetadata: {
+          name: "Peach Pool Dapp",
+          url: window.location.href,
+        },
+        infuraAPIKey: "d57e9ed1269046b3b0b6972c2a667866",
+        // Other options
+      }}
+    >
+      <App />
+    </MetaMaskProvider>
   </React.StrictMode>
 );
 
