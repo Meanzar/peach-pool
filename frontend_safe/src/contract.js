2cec1ed088d7827f1e3698240f87ad58ae94e505
meanzar <116259100+Meanzar@users.noreply.github.com>
2025-06-07 17:59:42 +0200

    fix build peer dependecies issues

--- frontend_safe/src/contract.js
+++ frontend_safe/src/contract.js
@@ -5,12 +5,11 @@ const abi = VotePoolJson.abi;
 const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
 
 export async function getContract() {
-    console.log("Récupération du contrat à l'adresse", CONTRACT_ADDRESS);
   if (!window.ethereum) throw new Error("MetaMask non installé");
   
   await window.ethereum.request({ method: "eth_requestAccounts" });
   const provider = new ethers.BrowserProvider(window.ethereum);
   const signer = await provider.getSigner();
-  console.log("Signer récupéré:", signer);
+  
   return new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
 }
